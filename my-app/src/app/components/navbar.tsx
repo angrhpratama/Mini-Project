6bc508577bca5a510111997cca59ff1249a2d342
angrhpratama <angrhpratama0@gmail.com>
2024-04-15 01:35:25 +0800

    add logo and search bar

--- my-app/src/app/components/navbar.tsx
+++ my-app/src/app/components/navbar.tsx
@@ -1,17 +1,63 @@
 'use client'
 
-import { Box, Flex } from '@chakra-ui/react'
+import { Search2Icon, SearchIcon } from '@chakra-ui/icons'
+import { Box, Button, ButtonGroup, Center, Flex, Icon, IconButton, Image, Input, InputGroup, InputLeftElement, MenuButton, Spacer } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    
-    <Flex>
-        <a> Ticket</a>
-        <a> Register</a>
-        <a> Porfile</a>
 
-    </Flex>
+    <Flex
+      alignItems='center'
+      bg='black'
+      p='10px'>
+
+      <Box ml='50px'>
+        <Image
+          w='90px'
+          h='40px'
+          src='https://res.cloudinary.com/dhioc6nup/image/upload/v1713109589/mini%20project/yahmax8x0aq3sf9zj7ya.png' alt='Gambar Logo' />
+      </Box>
+
+      <Box ml='35px'>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none"
+            children={<SearchIcon color="grey" padding="4px" w='10px' ml='2' />} />
+          <Input
+            w='300px'
+            h='25px'
+            type='search'
+            color='grey.700'
+            fontSize='x-small'
+            placeholder='Search by artist, event or venue' padding="0 20px" />
+        </InputGroup>
+      </Box>
+
+      <Box
+        ml='150px'>
+
+        <ButtonGroup gap='2'>
+          <Button colorScheme='teal'>Sign Up</Button>
+          <Button colorScheme='teal'>Log in</Button>
+        <Spacer />
+        </ButtonGroup>
+
+        <Button colorScheme='white' >
+          Ticket
+        </Button>
+
+        <Button colorScheme='teal' variant='outline'>
+          Register
+        </Button>
+
+        <Button colorScheme='white' variant='outline' borderColor='white'>
+          Create Event
+        </Button>
+
+
+      </Box>
+
+    </Flex >
 
 
   )
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -7,19 +7,13 @@ import { SearchIcon } from "@chakra-ui/icons";
 export default function Home() {
   return (
     <div>
-      <Box color='white' bg='black'>
-        Menu
 
-        <InputGroup>
-        <InputLeftElement  pointerEvents="none"
-        children={<SearchIcon color="blue.500" />} />
-          <Input w='300px' placeholder='Search by artist, event or venue' padding="0 30px" />
-        </InputGroup>
-
-
-      </Box>
 
       <Navbar />
+        
+      
+      Menu
+
 
 
     </div>
